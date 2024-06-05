a47021102ca2fbf209279b7f8a00315c782fb2d5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-05 19:19:45 +0200

    sketch

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,16 @@ int main(void)
 
     int n_devices = Pa_GetDeviceCount();
     std::cout << "number of devices: " << n_devices << '\n';
+    if (n_devices < 0)
+    {
+        std::cout << "Error getting device count\n";
+        exit(EXIT_FAILURE);
+    }
+    else if (n_devices == 0)
+    {
+        std::cout << "No audio device found\n";
+        exit(EXIT_FAILURE);
+    }
 
     err = Pa_Terminate();
     check_error(err);
@@ -23,7 +33,7 @@ static void check_error(PaError err)
 {
     if (err != paNoError)
     {
-        std::cout << "Portaudio error: " << Pa_GetErrorText(err);
+        std::cout << "Portaudio error: " << Pa_GetErrorText(err) << '\n';
         exit(EXIT_FAILURE);
     }
 }
