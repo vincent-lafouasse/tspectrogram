1cecbb77dcc3cebc65c5573a7728bca2f3043dc9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-05 19:10:15 +0200

    sketch

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <portaudio.h>
+#include <cstdlib>
+#include <iostream>
+
+static void check_error(PaError err);
 
 int main(void)
 {
-    PaError err = Pa_Initialize();
+    PaError err;
+    err = Pa_Initialize();
+    check_error(err);
+
+    err = Pa_Terminate();
+    check_error(err);
+
+    return EXIT_SUCCESS;
+}
+
+static void check_error(PaError err)
+{
+    if (err != paNoError)
+    {
+        std::cout << "Portaudio error: " << Pa_GetErrorText(err);
+        exit(EXIT_FAILURE);
+    }
 }
