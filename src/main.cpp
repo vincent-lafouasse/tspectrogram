bd9751ce062f7f9ee01533c1bd59cacefbfcc617
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-05 19:52:20 +0200

    init stream

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,18 @@ static int query_user_for_device();
 static void log_devices(int n_devices);
 static void check_error(PaError err);
 
+constexpr int sample_rate = 44100;
+constexpr unsigned long buffer_size = 512;
+
+static int input_callback(const void* input_buffer,
+                          void* output_buffer,
+                          unsigned long buffer_size,
+                          const PaStreamCallbackTimeInfo* time_info,
+                          PaStreamCallbackFlags status_flags,
+                          void* user_data)
+{
+}
+
 int main(void)
 {
     PaError err;
@@ -22,13 +34,36 @@ int main(void)
     std::memset(&input_params, 0, sizeof(input_params));
     std::memset(&output_params, 0, sizeof(output_params));
 
-    input_params.channelCount = 2;
+    input_params.channelCount = 1;
     input_params.device = device;
     input_params.hostApiSpecificStreamInfo = NULL;
     input_params.sampleFormat = paFloat32;
     input_params.suggestedLatency =
         Pa_GetDeviceInfo(device)->defaultLowInputLatency;
 
+    output_params.channelCount = 1;
+    output_params.device = device;
+    output_params.hostApiSpecificStreamInfo = NULL;
+    output_params.sampleFormat = paFloat32;
+    output_params.suggestedLatency =
+        Pa_GetDeviceInfo(device)->defaultLowOutputLatency;
+
+    PaStream* stream;
+    err = Pa_OpenStream(&stream, &input_params, &output_params, sample_rate,
+                        buffer_size, paNoFlag, input_callback, NULL);
+    check_error(err);
+
+    err = Pa_StartStream(stream);
+    check_error(err);
+
+    int capture_time_secs = 5;
+    Pa_Sleep(capture_time_secs * 1000);
+
+    err = Pa_StopStream(stream);
+    check_error(err);
+
+    err = Pa_CloseStream(stream);
+    check_error(err);
     err = Pa_Terminate();
     check_error(err);
 
