b1bb4249ab933e031ed60d7c0204aa39da37e6d6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-08 22:38:54 +0200

    sketch

--- src/main.c
+++ src/main.c
@@ -1,59 +1,56 @@
-#include <algorithm>
-#include <chrono>
-#include <cstdlib>
-#include <iostream>
-#include <numeric>
-#include <thread>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
 
 #include "InputStream.h"
 #include "fftw3.h"
 
-constexpr int sample_rate = 48000;
-constexpr unsigned long buffer_size = 512;
-constexpr int n_channels = 1;
+#define SAMPLE_RATE 48000.0
+#define BUFFER_SIZE 512
+#define N_CHANNELS 1
 
-constexpr float min_frequency = 20.0;
-constexpr float max_frequency = 20000.0;
-constexpr float sensibility = 0.4;
+#define MIN_FREQUENCY 20.0
+#define MAX_FREQUENCY 20000.0
+#define SENSIBILITY 0.4
 
 // Data for a 1d real to real fft (r2r_1d) hence two double buffers
-struct FFTData
+typedef struct
 {
-    FFTData();
-    ~FFTData();
     double* dtft_input;
     double* dtft_output;
     fftw_plan plan;
     int start_index;  // represents 20 Hz, ie ignore information below 20 Hz
     int spectrogram_size;  // number of frequency buckets to fetch
-};
+} FFTData;
 
-FFTData::FFTData()
+static int min_int(int a, int b)
 {
-    dtft_input =
-        static_cast<double*>(fftw_malloc(buffer_size * sizeof(double)));
-    dtft_output =
-        static_cast<double*>(fftw_malloc(buffer_size * sizeof(double)));
-
-    constexpr unsigned int plan_flags = FFTW_ESTIMATE;
-    constexpr fftw_r2r_kind kind = FFTW_HC2R;
-    plan = fftw_plan_r2r_1d(buffer_size, dtft_input, dtft_output, kind,
-                            plan_flags);
-
-    constexpr double sample_ratio =
-        buffer_size / static_cast<double>(sample_rate);
-    const int end_index =
-        std::min(static_cast<int>(buffer_size),
-                 static_cast<int>(std::ceil(max_frequency * sample_ratio)));
-    start_index = std::ceil(min_frequency * sample_ratio);
-    spectrogram_size = end_index - start_index;
+    return a > b ? b : a;
 }
 
-FFTData::~FFTData()
+FFTData fft_data_init(void)
 {
-    fftw_free(dtft_input);
-    fftw_free(dtft_output);
-    fftw_destroy_plan(plan);
+    FFTData data;
+    data.dtft_input = (double*)fftw_malloc(BUFFER_SIZE * sizeof(double));
+    data.dtft_output = (double*)fftw_malloc(BUFFER_SIZE * sizeof(double));
+
+    const unsigned int plan_flags = FFTW_ESTIMATE;
+    const fftw_r2r_kind kind = FFTW_HC2R;
+    data.plan = fftw_plan_r2r_1d(BUFFER_SIZE, data.dtft_input, data.dtft_output, kind,
+                            plan_flags);
+
+    const double sample_ratio = BUFFER_SIZE / SAMPLE_RATE;
+    const int end_index =
+        min_int(BUFFER_SIZE, ceil(MAX_FREQUENCY * sample_ratio));
+    data.start_index = ceil(MIN_FREQUENCY * sample_ratio);
+    data.spectrogram_size = end_index - data.start_index;
+}
+
+void fft_data_destroy(FFTData* data)
+{
+    fftw_free(data.dtft_input);
+    fftw_free(data.dtft_output);
+    fftw_destroy_plan(data.plan);
 }
 
 static int mono_spectrogram(const void* input_buffer,
